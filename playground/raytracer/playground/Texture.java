aa3d962dfb39161ae3e8e6f9a22450b13e6ee104
hippokrates1337 <j.weber_2003@web.de>
2020-05-04 00:19:53 +0200

    ray tracer working

--- playground/raytracer/playground/Texture.java
+++ playground/raytracer/playground/Texture.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Texture {
-	public static Texture wood = new Texture("res/Texture 4.png", 64);
-	public static Texture brick = new Texture("res/Texture 2.png", 64);
-	public static Texture bluestone = new Texture("res/Texture 3.png", 64);
-	public static Texture stone = new Texture("res/Texture 1.png", 64);
+	public static Texture wood = new Texture("C:\\Users\\Joachim\\git\\LearningJava\\playground\\raytracer\\playground\\res\\Texture 4.png", 64);
+	public static Texture brick = new Texture("C:\\Users\\Joachim\\git\\LearningJava\\playground\\raytracer\\playground\\res\\Texture 2.png", 64);
+	public static Texture bluestone = new Texture("C:\\Users\\Joachim\\git\\LearningJava\\playground\\raytracer\\playground\\res\\Texture 3.png", 64);
+	public static Texture stone = new Texture("C:\\Users\\Joachim\\git\\LearningJava\\playground\\raytracer\\playground\\res\\Texture 1.png", 64);
 	public int[] pixels;
 	private String loc;
 	public final int SIZE;
